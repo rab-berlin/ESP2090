fec33f4696f04d9ba1aa5eec33da2d35613f1a4e
rab-berlin <165693075+rab-berlin@users.noreply.github.com>
2026-04-04 12:29:06 +0200

    Add files via upload

---
+++ program/berlinuhr/farbmatrix.py
@@ -0,0 +1,14 @@
+import uasyncio as asyncio
+
+color_matrix = [
+    ["rot","rot","rot","rot","rot","rot","rot","rot"],
+    ["schwarz","schwarz","schwarz","schwarz","schwarz","schwarz","schwarz","schwarz"],
+    ["rot","rot","rot","rot","rot","rot","rot","rot"],
+    ["schwarz","schwarz","schwarz","schwarz","schwarz","schwarz","schwarz","schwarz"],
+    ["orange","orange","rot","orange","orange","schwarz","schwarz","blau"],
+    ["orange","orange","rot","orange","orange","rot","schwarz","schwarz"],
+    ["schwarz","schwarz","schwarz","schwarz","schwarz","schwarz","schwarz","schwarz"],
+    ["orange","orange","orange","orange","orange","orange","orange","orange"],
+]
+
+setColors(color_matrix)
